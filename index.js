e985b66388c69126d2ab22787274721f569abb08
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-05-17 11:30:09 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function getCatInfo() {
+  fetch("https://aws.random.cat/meow")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(result) {
+      const catContainer = document.getElementById("catContainer");
+      const catInfoBox = document.createElement("section");
+      const catImage = document.createElement("img");
+      catImage.src = result.file;
+      catInfoBox.append(catImage);
+      catContainer.prepend(catInfoBox);
+    });
+}
+
+console.log(document.getElementById("getCatInfo"));
+document.getElementById("getCatInfo").onclick = getCatInfo;
